d2160cadc93feb5b6e73bc4b6d1b9cbd162d844a
Jan Jaekel <jan.jaekel@tu-dortmund.de>
2017-12-02 09:39:49 +0100

    added auswertung schwingungsdauer

--- plot.py
+++ plot.py
@@ -1,27 +1,39 @@
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
+from scipy.optimize import curve_fit
+import scipy.constants.constants as const
+from uncertainties import ufloat
+from uncertainties import unumpy
+
 
 mpl.use('pgf')
 mpl.rcParams.update({
     'pgf.preamble': r'\usepackage{siunitx}',
 })
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+x, y = np.genfromtxt('content/Messwerte_schwing.txt', unpack=True)
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
+plt.plot(x, y, 'rx')
+plt.xlabel(r'$1/B[\si{1\per\tesla}]$')
+plt.ylabel(r'$T^2[\si{\second\squared}]$')
+plt.grid(True, which='both')
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
 
-# in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot.pdf')
+# Fitvorschrift
+def f(x, A, B):
+    return A*x + B      #jeweilige Fitfunktion auswaehlen:
+
+params, covar = curve_fit(f, x, y)            #eigene Messwerte hier uebergeben
+uparams = unumpy.uarray(params, np.sqrt(np.diag(covar)))
+for i in range(0, len(uparams)):
+    print(chr(ord('A') + i), "=" , uparams[i])
+print()
+
+plt.plot(x, f(x, *params), "b--", label=r'Regression' )
+
+
+
+plt.tight_layout()
+plt.savefig('schwing.pdf')
+plt.clf()
